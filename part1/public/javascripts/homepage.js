60aacfd236aeb3a5f573fd1dee6686a9a69488b3
Felix <a1878510@adelaide.edu.au>
2023-06-15 12:17:46 +0000

    [automatic commit] 2023-06-15 12:17:46 UTC 202.7.248.234

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -12,14 +12,13 @@ var homepage = new Vue({
         chatHistory: []
     },
     methods: {
-        openContactForm: function(product) {
+        openContactForm: function (product) {
             this.selectedISBN = product.ISBN;
             this.seller_name = product.seller_name;
             this.seller_id = product.seller_id;
             this.user_id = this.selectedUser;
             this.showForm = true;
-            this.getChatHistory();
-            this.checkIfSeller(this.selectedUser);
+            this.checkIfSeller(this.user_id);
         },
         checkIfSeller: function (user_id) {
             fetch('/checkIfSeller', {
@@ -35,66 +34,86 @@ var homepage = new Vue({
                 .then((data) => {
                     if (data.length === 0) {
                         console.log('The user is not a seller.');
+                        this.getChatHistory();
                     } else {
                         console.log('The user is a seller.');
+                        this.getSellerChatHistory();
                     }
                 })
                 .catch((error) => {
                     console.error('Error:', error);
                 });
         },
-
-            sendMessage: function() {
-        fetch('/contactSeller', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                ISBN: this.selectedISBN,
-                message: this.message,
-                user_id: this.user_id,
-                seller_id: this.seller_id
-            }),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-            this.showForm = false;
-            this.getChatHistory();
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-        this.selectedISBN = '';
-        this.message = '';
-        this.seller = '';
-    },
-        getChatHistory: function() {
-    fetch('/getChatHistory', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+        sendMessage: function () {
+            fetch('/contactSeller', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    ISBN: this.selectedISBN,
+                    message: this.message,
+                    user_id: this.user_id,
+                    seller_id: this.seller_id
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Success:', data);
+                    this.showForm = false;
+                    this.getChatHistory();
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            this.selectedISBN = '';
+            this.message = '';
+            this.seller = '';
         },
-        body: JSON.stringify({
-            user_id: this.user_id
-        }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Success:', data);
-        this.chatHistory = data;
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-},
+        getChatHistory: function () {
+            fetch('/getChatHistory', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user_id: this.user_id
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Success:', data);
+                    this.chatHistory = data;
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        },
+        getSellerChatHistory: function () {
+            fetch('/getSellerChatHistory', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    seller_id: this.user_id
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Success:', data);
+                    this.chatHistory = data;
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        },
     },
     watch: {
-        selectedUser: function(newUserId, oldUserId) {
+        selectedUser: function (newUserId, oldUserId) {
             this.user_id = newUserId;
             if (this.showForm) {
-                this.getChatHistory();
+                this.checkIfSeller(this.user_id);
             }
         }
     },
