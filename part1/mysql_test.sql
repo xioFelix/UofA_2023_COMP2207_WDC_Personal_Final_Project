9a18affb8a92af982e9ba1f561974e7df6e7d675
Felix <a1878510@adelaide.edu.au>
2023-06-15 03:19:56 +0000

    [automatic commit] 2023-06-15 03:19:56 UTC 202.7.248.234

--- part1/mysql_test.sql
+++ part1/mysql_test.sql
@@ -73,6 +73,6 @@ DELETE FROM table_name;
 
 SELECT Books.title, Books.author, Transitions.sale_date
 FROM Books
-JOIN Transitions ON b.ISBN = Transitions.ISBN
+JOIN Transitions ON Books.ISBN = Transitions.ISBN
 WHERE Transitions.user_id = 1 AND Transitions.sale_date >= CURDATE() - INTERVAL 30 DAY
 ORDER BY Transitions.sale_date DESC;
