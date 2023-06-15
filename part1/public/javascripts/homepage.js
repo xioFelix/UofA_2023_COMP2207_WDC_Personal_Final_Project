6d6c1b2f4f81e5ad1ba0496949e397c98d640300
Felix <a1878510@adelaide.edu.au>
2023-06-15 05:58:10 +0000

    [automatic commit] 2023-06-15 05:58:10 UTC 202.7.248.234

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -14,24 +14,3 @@ var homepage = new Vue({
             });
     }
 });
-
-
-window.onload = function () {
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var books = JSON.parse(this.responseText);
-            var booksDiv = document.getElementById('books');
-            for (var i = 0; i < books.length; i++) {
-                booksDiv.innerHTML += '<h2>' + books[i].title + '</h2>' +
-                    '<p>Author: ' + books[i].author + '</p>' +
-                    '<p>Price: ' + books[i].price + '</p>' +
-                    '<img src="' + books[i].image + '" alt="Book Image">';
-            }
-        }
-    };
-
-    xhttp.open("GET", "/allBooks", true);
-    xhttp.send();
-};
