ad23cf20c3510a1b2a12a4c669e04eaf56b8013f
Felix <a1878510@adelaide.edu.au>
2023-06-15 06:09:47 +0000

    [automatic commit] 2023-06-15 06:09:47 UTC 202.7.248.234

--- part1/app.js
+++ part1/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var Vue = require('vue');
-var mysql = require('mysql2');
+var mysql2 = require('mysql2');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-var pool = mysql.createPool({
+var pool = mysql2.createPool({
     host: 'localhost',
     database: 'marketplace'
 });
