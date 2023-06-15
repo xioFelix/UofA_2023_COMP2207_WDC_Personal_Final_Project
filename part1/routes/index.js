95a17ed6572fd98a0dd3d778023da25cdbffbeda
Felix <a1878510@adelaide.edu.au>
2023-06-15 11:01:21 +0000

    [automatic commit] 2023-06-15 11:01:21 UTC 202.7.248.234

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -81,9 +81,7 @@ router.post('/getChatHistory', function (req, res, next) {
       res.sendStatus(500);
       return;
     }
-    var query = "SELECT Messages.message_id, Messages.message, Messages.message_date, Users.user_name FROM Messages INNER JOIN Sellers ON Messages.seller_id = Sellers.seller_id INNER JOIN Users ON Sellers.user_id = Users.user_id WHERE Messages.user_id = ?  -- 这里的问号表示的是你查询时传入的具体 user_id
-ORDER BY Messages.message_date DESC;
-";
+    var query = "SELECT Messages.message_id, Messages.message, Messages.message_date, Users.user_name FROM Messages INNER JOIN Sellers ON Messages.seller_id = Sellers.seller_id INNER JOIN Users ON Sellers.user_id = Users.user_id WHERE Messages.user_id = ? user_id ORDER BY Messages.message_date DESC;";
     var params = [req.body.user_id];
     connection.query(query, params, function (err, result) {
       connection.release();
