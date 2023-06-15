e2d7f2c9f9c1ebad3bcb3fb7fd4a227d6651ac4c
Felix <a1878510@adelaide.edu.au>
2023-06-15 01:18:03 +0000

    [automatic commit] 2023-06-15 01:18:02 UTC 202.7.248.234

--- part1/mysql.sql
+++ part1/mysql.sql
@@ -6,6 +6,14 @@ CREATE TABLE Books (
     PRIMARY KEY (ISBN)
 );
 
+CREATE TABLE Adveritsement (
+    ISBN varchar(255) NOT NULL,
+    title varchar(255) NOT NULL,
+    auther varchar(255) NOT NULL,
+    price varchar(255) NOT NULL,
+    PRIMARY KEY (ISBN)
+);
+
 CREATE TABLE Books (
     ISBN varchar(255) NOT NULL,
     title varchar(255) NOT NULL,
