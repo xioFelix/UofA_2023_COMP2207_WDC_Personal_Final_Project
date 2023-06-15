a81065c2d68a1ddf301207de439db3520423871f
Felix <a1878510@adelaide.edu.au>
2023-06-15 07:08:52 +0000

    [automatic commit] 2023-06-15 07:08:52 UTC 202.7.248.234

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -1,7 +1,36 @@
 var homepage = new Vue({
     el: "#products",
     data: {
-        products: []
+        products: [],
+        selectedISBN: '',  // 新增用于保存当前选中的图书ISBN
+        message: ''  // 新增用于保存用户输入的消息
+    },
+    methods: {
+        openContactForm: function(ISBN) {
+            this.selectedISBN = ISBN;  // 保存当前选中的图书ISBN
+        },
+        sendMessage: function() {
+            // 在这里，你可以使用 AJAX 将消息发送到服务器
+            fetch('/contactSeller', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    ISBN: this.selectedISBN,
+                    message: this.message
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+            this.selectedISBN = '';
+            this.message = '';
+        },
     },
     mounted: function () {
         fetch('/allBooks')
