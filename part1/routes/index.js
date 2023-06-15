220c58d5f1c8d17e41b52555de97f79feb8e8605
Felix <a1878510@adelaide.edu.au>
2023-06-15 05:49:17 +0000

    [automatic commit] 2023-06-15 05:49:17 UTC 202.7.248.234

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -21,7 +21,7 @@ router.get('/result', function (req, res, next) {
         res.sendStatus(500);
         return;
       }
-      res.json(rows); //send response
+      res.json(rows);
     });
   });
 });
