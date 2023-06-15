65f56ac1b59d4a51335446913c23fa86050b97ae
Felix <a1878510@adelaide.edu.au>
2023-06-15 06:11:09 +0000

    [automatic commit] 2023-06-15 06:11:09 UTC 202.7.248.234

--- part1/app.js
+++ part1/app.js
@@ -15,16 +15,16 @@ var pool = mysql2.createPool({
     database: 'marketplace'
 });
 
-app.use(function (req, res, next) {
-    req.pool = pool;
-    next();
-});
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    req.pool = pool;
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
