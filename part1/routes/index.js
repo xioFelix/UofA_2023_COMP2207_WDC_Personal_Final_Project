ce53ef738e085c9d64237fdc014137155dccdae6
Felix <a1878510@adelaide.edu.au>
2023-06-15 05:49:38 +0000

    [automatic commit] 2023-06-15 05:49:38 UTC 202.7.248.234

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/result', function (req, res, next) {
+router.get('/products', function (req, res, next) {
   //Connect to the database
   req.pool.getConnection(function (err, connection) {
     if (err) {
@@ -15,8 +15,8 @@ router.get('/result', function (req, res, next) {
       return;
     }
     var query = "SHOW TABLES";
-    connection.query(query, function (err, rows, fields) {
-      connection.release(); // release connection
+    connection.query(query, function (rows, fields) {
+      connection.release();
       if (err) {
         res.sendStatus(500);
         return;
