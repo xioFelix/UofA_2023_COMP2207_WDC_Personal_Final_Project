cf9accfe48de1b7b84c306fa93f6138e0ada2407
Felix <a1878510@adelaide.edu.au>
2023-06-15 08:15:02 +0000

    [automatic commit] 2023-06-15 08:15:02 UTC 202.7.248.234

--- part1/part1.sql
+++ part1/part1.sql
@@ -100,6 +100,7 @@ CREATE TABLE `Messages` (
   KEY `seller_id` (`seller_id`),
   CONSTRAINT `Messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
   CONSTRAINT `Messages_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `Sellers` (`seller_id`)
+  FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
