379eebc7fae2decb36c1a27bf91c1b141fc744d2
Felix <a1878510@adelaide.edu.au>
2023-06-15 06:57:14 +0000

    [automatic commit] 2023-06-15 06:57:14 UTC 202.7.248.234

--- part1/part1.sql
+++ part1/part1.sql
@@ -137,3 +137,79 @@ CREATE TABLE `Sellers` (
 LOCK TABLES `Sellers` WRITE;
 /*!40000 ALTER TABLE `Sellers` DISABLE KEYS */;
 INSERT INTO `Sellers` VALUES (1,'Location 1',1),(2,'Location 2',2),(3,'Location 3',3);
+/*!40000 ALTER TABLE `Sellers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Transitions`
+--
+
+DROP TABLE IF EXISTS `Transitions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Transitions` (
+  `transition_id` int NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(255) NOT NULL,
+  `sale_date` date NOT NULL,
+  `user_id` int NOT NULL,
+  `seller_id` int NOT NULL,
+  PRIMARY KEY (`transition_id`),
+  UNIQUE KEY `transition_id` (`transition_id`),
+  KEY `ISBN` (`ISBN`),
+  KEY `user_id` (`user_id`),
+  KEY `seller_id` (`seller_id`),
+  CONSTRAINT `Transitions_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`),
+  CONSTRAINT `Transitions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
+  CONSTRAINT `Transitions_ibfk_3` FOREIGN KEY (`seller_id`) REFERENCES `Sellers` (`seller_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Transitions`
+--
+
+LOCK TABLES `Transitions` WRITE;
+/*!40000 ALTER TABLE `Transitions` DISABLE KEYS */;
+INSERT INTO `Transitions` VALUES (1,'978-3-16-148410-0','2023-06-15',1,1),(2,'978-3-16-148410-1','2023-05-16',2,2),(3,'978-3-16-148410-2','2023-04-17',3,3),(4,'978-3-16-148410-0','2023-06-15',1,1),(5,'978-3-16-148410-0','2023-05-20',2,1),(6,'978-3-16-148410-0','2023-06-01',3,1),(7,'978-3-16-148410-1','2023-05-01',2,2),(8,'978-3-16-148410-1','2023-05-01',3,2),(9,'978-3-16-148410-2','2023-06-15',1,3),(10,'978-3-16-148410-2','2023-04-17',3,3),(11,'978-3-16-148410-0','2023-06-15',2,1),(12,'978-3-16-148410-0','2023-05-20',3,1),(13,'978-3-16-148410-0','2023-06-01',1,1),(14,'978-3-16-148410-1','2023-05-01',3,2),(15,'978-3-16-148410-1','2023-05-01',2,2),(16,'978-3-16-148410-2','2023-06-15',2,3),(17,'978-3-16-148410-2','2023-04-17',1,3);
+/*!40000 ALTER TABLE `Transitions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'123','user1','user1@email.com'),(2,'456','user2','user2@email.com'),(3,'789','user3','user3@email.com');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-15  6:56:54
