4c47532cf7424d0d6bd44210dcf5ff350084ce9b
Felix <a1878510@adelaide.edu.au>
2023-06-15 13:16:39 +0000

    [automatic commit] 2023-06-15 13:16:39 UTC 202.7.248.234

--- part2/.eslintrc.js
+++ part2/.eslintrc.js
@@ -1,19 +1,5 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-    },
-    plugins: [
-        "vue"
-    ],
-    rules: {
-    }
+  extends: [
+    'semistandard'
+  ]
 };
