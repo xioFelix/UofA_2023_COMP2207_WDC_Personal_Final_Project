9daacb6381d84991422583015545f9aaad2d67ad
Felix <a1878510@adelaide.edu.au>
2023-06-15 01:27:04 +0000

    [automatic commit] 2023-06-15 01:27:04 UTC 202.7.248.234

--- part1/mysql.sql
+++ part1/mysql.sql
@@ -7,7 +7,7 @@ CREATE TABLE Books (
 );
 
 CREATE TABLE Users (
-    user_id varchar(255) NOT NULL AUTO_INCREMENT UNIQUE,
+    user_id int NOT NULL AUTO_INCREMENT UNIQUE,
     password varchar(255) NOT NULL,
     user_name varchar(255) NOT NULL,
     user_email varchar(255) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Sellers (
-    user_id varchar(255) NOT NULL AUTO_INCREMENT UNIQUE,
+    seller_id varchar(255) NOT NULL AUTO_INCREMENT UNIQUE,
     password varchar(255) NOT NULL,
     user_name varchar(255) NOT NULL,
     user_email varchar(255) NOT NULL,
