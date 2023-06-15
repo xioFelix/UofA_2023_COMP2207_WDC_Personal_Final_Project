a8e9c21ce950c57dd55798767e39d8659923de9b
Felix <a1878510@adelaide.edu.au>
2023-06-15 08:53:24 +0000

    [automatic commit] 2023-06-15 08:53:24 UTC 202.7.248.234

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -14,6 +14,7 @@ var homepage = new Vue({
         openContactForm: function(product) {
             this.selectedISBN = product.ISBN;
             this.seller_name = product.seller_name;
+            this.seller_id = product.seller_nam;
             // Set user_id from the selected user
             this.user_id = this.selectedUser;
             this.showForm = true;
