c31b2d1825297e329f9253628a370c87b9d6b737
Felix <a1878510@adelaide.edu.au>
2023-06-15 11:17:45 +0000

    [automatic commit] 2023-06-15 11:17:45 UTC 202.7.248.234

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -93,3 +93,35 @@ var homepage = new Vue({
             });
     }
 });
+
+var dragElement = document.getElementById("floating-panel");
+
+dragElement.onmousedown = function(event) {
+  // 在 mousedown 事件中，记录下鼠标的初始位置
+  var startX = event.clientX;
+  var startY = event.clientY;
+
+  var rect = dragElement.getBoundingClientRect();
+  // 记录下元素的初始位置
+  var startLeft = rect.left;
+  var startTop = rect.top;
+
+  function onMouseMove(event) {
+    // 在 mousemove 事件中，计算鼠标移动的距离，并改变元素的位置
+    var shiftX = event.clientX - startX;
+    var shiftY = event.clientY - startY;
+
+    dragElement.style.left = startLeft + shiftX + 'px';
+    dragElement.style.top = startTop + shiftY + 'px';
+  }
+
+  // 在 mousedown 事件中，添加 mousemove 和 mouseup 事件监听器
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', function() {
+    // 在 mouseup 事件中，移除 mousemove 和 mouseup 事件监听器
+    document.removeEventListener('mousemove', onMouseMove);
+  });
+
+  // 阻止默认事件，避免选中 dragElement 内的文本时发生拖动
+  event.preventDefault();
+};
