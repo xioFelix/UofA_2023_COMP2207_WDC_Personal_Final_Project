8837e574449804daef8e00639e0aa899f4a3446c
Felix <a1878510@adelaide.edu.au>
2023-06-15 03:28:38 +0000

    [automatic commit] 2023-06-15 03:28:38 UTC 202.7.248.234

--- part1/mysql_test.sql
+++ part1/mysql_test.sql
@@ -80,3 +80,10 @@ FROM Books
 JOIN Transitions ON Books.ISBN = Transitions.ISBN
 WHERE Transitions.user_id = 1 AND Transitions.sale_date >= CURDATE() - INTERVAL 30 DAY
 ORDER BY Transitions.sale_date DESC;
+
+SELECT b.title, b.author, COUNT(*) as traded_count
+FROM Books
+JOIN Transitions t ON b.ISBN = t.ISBN
+GROUP BY b.ISBN
+ORDER BY traded_count DESC
+LIMIT 10;
