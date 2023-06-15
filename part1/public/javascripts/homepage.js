0933468bd0ae1e5ecae8d01b3e7196725e8eb188
Felix <a1878510@adelaide.edu.au>
2023-06-15 11:30:24 +0000

    [automatic commit] 2023-06-15 11:30:24 UTC 202.7.248.234

--- part1/public/javascripts/homepage.js
+++ part1/public/javascripts/homepage.js
@@ -20,32 +20,32 @@ var homepage = new Vue({
             this.showForm = true;
             this.getChatHistory();
         },
-        sendMessage: function() {
-    fetch('/contactSeller', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            ISBN: this.selectedISBN,
-            message: this.message,
-            user_id: this.user_id,
-            seller_id: this.seller_id
-        }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        this.showForm = false;
-        this.getChatHistory(); // 在这里添加
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-    this.selectedISBN = '';
-    this.message = '';
-    this.seller = '';
-},
+            sendMessage: function() {
+        fetch('/contactSeller', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                ISBN: this.selectedISBN,
+                message: this.message,
+                user_id: this.user_id,
+                seller_id: this.seller_id
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            this.showForm = false;
+            this.getChatHistory();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+        this.selectedISBN = '';
+        this.message = '';
+        this.seller = '';
+    },
         getChatHistory: function() {
     fetch('/getChatHistory', {
         method: 'POST',
