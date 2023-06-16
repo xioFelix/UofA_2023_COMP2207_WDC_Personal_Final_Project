5e2f13e197ff9df9ed5980099fe3bdd26abd4b59
Felix <a1878510@adelaide.edu.au>
2023-06-16 05:33:45 +0000

    [automatic commit] 2023-06-16 05:33:45 UTC 202.7.248.234

---
+++ part1/part1.sql
@@ -0,0 +1,219 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: marketplace
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `marketplace`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `marketplace` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `marketplace`;
+
+--
+-- Table structure for table `Ads`
+--
+
+DROP TABLE IF EXISTS `Ads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Ads` (
+  `ad_id` int NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(255) NOT NULL,
+  `seller_id` int NOT NULL,
+  PRIMARY KEY (`ad_id`),
+  UNIQUE KEY `ad_id` (`ad_id`),
+  KEY `ISBN` (`ISBN`),
+  KEY `seller_id` (`seller_id`),
+  CONSTRAINT `Ads_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`),
+  CONSTRAINT `Ads_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `Sellers` (`seller_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ads`
+--
+
+LOCK TABLES `Ads` WRITE;
+/*!40000 ALTER TABLE `Ads` DISABLE KEYS */;
+INSERT INTO `Ads` VALUES (1,'978-3-16-148410-0',1),(2,'978-3-16-148410-1',2),(3,'978-3-16-148410-2',3);
+/*!40000 ALTER TABLE `Ads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Books`
+--
+
+DROP TABLE IF EXISTS `Books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Books` (
+  `ISBN` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `image` varchar(255) DEFAULT '/images/default.png',
+  PRIMARY KEY (`ISBN`),
+  UNIQUE KEY `ISBN` (`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Books`
+--
+
+LOCK TABLES `Books` WRITE;
+/*!40000 ALTER TABLE `Books` DISABLE KEYS */;
+INSERT INTO `Books` VALUES ('978-3-16-148410-0','Book 1','Author 1',0.99,'/images/1.png'),('978-3-16-148410-1','Book 2','Author 2',99.99,'/images/2.png'),('978-3-16-148410-2','Book 3','Author 3',50.00,'/images/3.png');
+/*!40000 ALTER TABLE `Books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Messages`
+--
+
+DROP TABLE IF EXISTS `Messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Messages` (
+  `message_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `seller_id` int NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `message_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ISBN` varchar(255) NOT NULL,
+  PRIMARY KEY (`message_id`),
+  UNIQUE KEY `message_id` (`message_id`),
+  KEY `user_id` (`user_id`),
+  KEY `seller_id` (`seller_id`),
+  KEY `ISBN` (`ISBN`),
+  CONSTRAINT `Messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
+  CONSTRAINT `Messages_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `Sellers` (`seller_id`),
+  CONSTRAINT `Messages_ibfk_3` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Messages`
+--
+
+LOCK TABLES `Messages` WRITE;
+/*!40000 ALTER TABLE `Messages` DISABLE KEYS */;
+INSERT INTO `Messages` VALUES (1,1,2,'User 1 contact with Seller 2','2023-06-16 04:42:40','978-3-16-148410-1'),(3,2,1,'User 2 contact with Seller 1','2023-06-16 04:43:11','978-3-16-148410-0'),(5,1,3,'User 1 contact with Seller 3','2023-06-16 04:43:34','978-3-16-148410-2'),(8,2,3,'User 2 contact with Seller 3','2023-06-16 04:44:32','978-3-16-148410-2'),(9,3,1,'User 3 contact with Seller 1','2023-06-16 04:45:01','978-3-16-148410-0'),(10,3,2,'User 3 contact with Seller 2','2023-06-16 04:45:07','978-3-16-148410-1');
+/*!40000 ALTER TABLE `Messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Sellers`
+--
+
+DROP TABLE IF EXISTS `Sellers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Sellers` (
+  `seller_id` int NOT NULL AUTO_INCREMENT,
+  `location` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`seller_id`),
+  UNIQUE KEY `seller_id` (`seller_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  CONSTRAINT `Sellers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Sellers`
+--
+
+LOCK TABLES `Sellers` WRITE;
+/*!40000 ALTER TABLE `Sellers` DISABLE KEYS */;
+INSERT INTO `Sellers` VALUES (1,'Location 1',4),(2,'Location 2',5),(3,'Location 3',6);
+/*!40000 ALTER TABLE `Sellers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Transitions`
+--
+
+DROP TABLE IF EXISTS `Transitions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Transitions` (
+  `transition_id` int NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(255) NOT NULL,
+  `sale_date` date NOT NULL,
+  `user_id` int NOT NULL,
+  `seller_id` int NOT NULL,
+  PRIMARY KEY (`transition_id`),
+  UNIQUE KEY `transition_id` (`transition_id`),
+  KEY `ISBN` (`ISBN`),
+  KEY `user_id` (`user_id`),
+  KEY `seller_id` (`seller_id`),
+  CONSTRAINT `Transitions_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`),
+  CONSTRAINT `Transitions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
+  CONSTRAINT `Transitions_ibfk_3` FOREIGN KEY (`seller_id`) REFERENCES `Sellers` (`seller_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Transitions`
+--
+
+LOCK TABLES `Transitions` WRITE;
+/*!40000 ALTER TABLE `Transitions` DISABLE KEYS */;
+INSERT INTO `Transitions` VALUES (1,'978-3-16-148410-0','2023-06-15',1,1),(2,'978-3-16-148410-1','2023-05-16',2,2),(3,'978-3-16-148410-2','2023-04-17',3,3),(4,'978-3-16-148410-0','2023-06-15',1,1),(5,'978-3-16-148410-0','2023-05-20',2,1),(6,'978-3-16-148410-0','2023-06-01',3,1),(7,'978-3-16-148410-1','2023-05-01',2,2),(8,'978-3-16-148410-1','2023-05-01',3,2),(9,'978-3-16-148410-2','2023-06-15',1,3),(10,'978-3-16-148410-2','2023-04-17',3,3),(11,'978-3-16-148410-0','2023-06-15',2,1),(12,'978-3-16-148410-0','2023-05-20',3,1),(13,'978-3-16-148410-0','2023-06-01',1,1),(14,'978-3-16-148410-1','2023-05-01',3,2),(15,'978-3-16-148410-1','2023-05-01',2,2),(16,'978-3-16-148410-2','2023-06-15',2,3),(17,'978-3-16-148410-2','2023-04-17',1,3);
+/*!40000 ALTER TABLE `Transitions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'123','user1','user1@email.com'),(2,'456','user2','user2@email.com'),(3,'789','user3','user3@email.com'),(4,'123','seller1','seller1@email.com'),(5,'123',' seller2','seller2@email.com'),(6,'123',' seller3','seller3@email.com');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-16  5:33:35
